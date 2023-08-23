5744e22b5a3a0db1c39165c0f89b6b8944be3948
Bruno Attina <bruattina@gmail.com>
2023-08-22 22:41:22 -0300

    Criando a conexão com o banco

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php 
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
 
+print_r(PDO::getAvailableDrivers());
 
 // Connect to our MySQK database.
 
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+class Database
+{
+    public $connection;
+    public function __construct()
+    {
 
-$pdo = new PDO($dsn);
+        $host = 'localhost';
+        $dbname = 'myapp';
+        $user = 'root';
+        $password = 'root';
+        $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;user=$user;password=$password;charset=utf8mb4";
 
-$pdo->prepare("SELECT * FROM posts");
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query()
+    {
+        $statement = $this->connection->prepare("SELECT * FROM posts");
+        $statement->execute();
+
+        return $statement->fetchAll();  
+    }
+}
+
+$db = new Database();
+dd( $db->query());
+//dd();
 
 
 /*
